593592a051cce62427f315034254247f880171c8
hs3verson <hvsev@login2.talapas.uoregon.edu>
2026-09-02 23:16:44 -0700

    Renamed R code for plots and answered part 2 analysis questions

--- Project2_Part2/cutadapt.sh
+++ Project2_Part2/cutadapt.sh
@@ -8,7 +8,7 @@
 
 cd /projects/bgmp/hvsev/bioinfo/Bi623/Projects/Project-2-Electric-organ-RNA-seq-analysis/Project2_QAA
 
-# Sample 1: SRR25630385 (a; R1 adapter, A; R2 adapter, o;output for R1, p; output for R2)
+# Sample 1: SRR25630385 (a; R1 adapter, A; piR2 adapter, o;output for R1, p; output for R2)
 /usr/bin/time -v pixi run cutadapt \
 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
 -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
